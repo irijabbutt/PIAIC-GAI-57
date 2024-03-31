baa23fa04082d3956bba1923be41810815fcf0aa
RIJAB BUTT <rijabbutt23@gmail.com>
2024-03-31 17:12:30 +0500

    Gen-AI Course

---
+++ Q1/typescript/conditional.ts
@@ -0,0 +1,26 @@
+// ---Conditional Statements---
+// --@ IF Else
+let cond = "Pakistan";
+let ages = 22;
+if (cond == "Pakistan") {
+	console.log("pakistani");
+} else {
+	console.log("not pakistani");
+}
+
+// if/else if
+if (ages <= 12 && ages < 13) {
+	console.log("child");
+} else if (ages >= 13 || ages <= 19) {
+	console.log("teenage");
+} else {
+	console.log("Adult");
+}
+
+// odd and even
+let result = ages % 2;
+if (result == 0) {
+	console.log("Even");
+} else {
+	console.log("odd");
+}
---
+++ Q1/typescript/variables.ts
@@ -0,0 +1,51 @@
+// Declare a variable for storing your favorite movie name with a string type annotation.
+let movie;
+movie = "Brave";
+console.log("1- ", movie);
+// Create a template string to introduce yourself, including your name and age.
+let nam_e = "Rijab Butt";
+let age = 21;
+let intro = `Hi, this is ${nam_e}. I'm ${age} years old, studying Generative AI from PIAIC Lahore.`;
+console.log("2- ", intro);
+// Initialize a constant with a type annotation for your current mood as a boolean.
+const current_mood = "Happy";
+console.log("3- ", current_mood, true);
+// Combine a user's first and last name using template literals and log the full name.
+let firstname = "Rijab";
+let lastname = "Butt";
+let fullname = `${firstname} ${lastname}`;
+console.log("4- ", fullname);
+// Create a variable for a user's age with a type annotation and assign a number to it.
+let user_age = "21";
+console.log("5- ", user_age);
+// Use a template string to display a product name and its price.
+let product_name = "Klean Beauty Gel Facewash";
+let price = "800Rs";
+let info = `Product: ${product_name}\n    Price: ${price}`;
+console.log("6- ", info);
+// Declare a variable to store the number of pets you have with a number type annotation.
+let pets = 1;
+console.log("7- ", pets);
+// Declare a variable with a boolean type annotation indicating if it is your birthday today.
+let bday = "3rd May";
+let date = "27th March";
+if (bday == date) {
+	console.log("8- ", "Birthday: ", true);
+} else {
+	console.log("8- ", "Birthday: ", false);
+}
+// Create a template string that includes a user's first name, last name, and age with proper labels.
+intro = `Firstname: ${firstname}\tLastname: ${lastname}\tAge: ${age}`;
+console.log("9- ", intro);
+// Initialize a constant to hold your birth year with a number type annotation.
+const birthyear = 2002;
+console.log("10- ", birthyear);
+// Write a line of code that uses a template string to combine a city and country into a full address.
+let city = "Lahore";
+let country = "Pakistan";
+let address = `H-645, Block-2, Sector-D1, ${city}, ${country}`;
+console.log("11- ", address);
+// Declare a variable for today's temperature with a number type annotation and assign a value to it.
+let temp = 29
+console.log('12- ',temp,'^C');
+
