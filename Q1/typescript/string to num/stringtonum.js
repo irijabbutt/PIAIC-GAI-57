60ce7b709bce25a32ca81f390a3135d9bf2a9b8f
RIJAB BUTT <rijabbutt23@gmail.com>
2024-04-21 20:16:59 +0500

    string to num concept

---
+++ Q1/typescript/string to num/stringtonum.js
@@ -0,0 +1,7 @@
+"use strict";
+let tonum;
+function stringto(tonum) {
+    let num = Number(tonum);
+    console.log(num, typeof (num));
+}
+stringto("87");
---
+++ Q1/typescript/string to num/stringtonum.ts
@@ -0,0 +1,8 @@
+let tonum
+function stringto(tonum: string)
+{
+    let num = Number(tonum)
+    console.log(num,typeof(num));
+    
+}
+stringto("87")
