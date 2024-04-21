3c7a6b9cbc235822e0de24546d3762b403d1b627
RIJAB BUTT <rijabbutt23@gmail.com>
2024-04-21 20:16:39 +0500

    functions in ts

---
+++ Q1/typescript/functions/functions.js
@@ -0,0 +1,8 @@
+"use strict";
+// Function is a block of code that performs specific task.
+let a, b;
+function sum(a, b) {
+    let resullt = a + b;
+    return resullt;
+}
+console.log(sum(2, 4));
---
+++ Q1/typescript/functions/functions.ts
@@ -0,0 +1,9 @@
+// Function is a block of code that performs specific task.
+let a, b;
+function sum(a: number, b: number) {
+	let resullt = a + b;
+
+	return resullt;
+}
+
+console.log(sum(2, 4));
